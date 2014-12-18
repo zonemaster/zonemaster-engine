6a354a996ac01d2d77faeebc7cb68f9253bc3671
Calle Dybedahl <calle@init.se>
2014-12-18 11:59:46 +0100

    Faked DS response packets should have AA set.

--- lib/Zonemaster/Nameserver.pm
+++ lib/Zonemaster/Nameserver.pm
@@ -122,7 +122,7 @@ sub query {
     # Fake a DS answer
     if ( $type eq 'DS' and $class eq 'IN' and $self->fake_ds->{ lc( $name ) } ) {
         my $p = Net::LDNS::Packet->new( $name, $type, $class );
-        $p->aa( 0 );
+        $p->aa( 1 );
         $p->do( $dnssec );
         $p->rd( $recurse );
         foreach my $rr ( @{ $self->fake_ds->{ lc( $name ) } } ) {
