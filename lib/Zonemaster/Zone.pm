11cc1f8069c59fef55b0807d9abbd6d1e99501a9
vlevigneron <vincent.levigneron@nic.fr>
2016-11-21 12:04:35 +0100

    - Fixes dotse/zonemaster-engine#239

--- lib/Zonemaster/Zone.pm
+++ lib/Zonemaster/Zone.pm
@@ -42,7 +42,7 @@ sub _build_glue_names {
         return [];
     }
 
-    my $p = $self->parent->query_one( $self->name, 'NS' );
+    my $p = $self->parent->query_persistent( $self->name, 'NS' );
 
     return [] if not defined $p;
 
