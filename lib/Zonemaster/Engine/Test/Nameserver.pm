471bc91020baa9ab7ee3ef5d80de1d3cf1a5ef8b
vlevigneron <vincent.levigneron@nic.fr>
2017-12-07 22:00:04 +0100

    - Change message error.

--- lib/Zonemaster/Engine/Test/Nameserver.pm
+++ lib/Zonemaster/Engine/Test/Nameserver.pm
@@ -1,6 +1,6 @@
 package Zonemaster::Engine::Test::Nameserver;
 
-use version; our $VERSION = version->declare("v1.0.11");
+use version; our $VERSION = version->declare("v1.0.12");
 
 use strict;
 use warnings;
@@ -141,7 +141,7 @@ sub translation {
         'QUERY_DROPPED'       => 'Nameserver {ns}/{address} dropped AAAA query.',
         'IS_A_RECURSOR'       => 'Nameserver {ns}/{address} is a recursor.',
         'NO_RECURSOR'         => 'None of the following nameservers is a recursor : {names}.',
-        'RECURSIVITY_UNDEF'   => 'Can not determine recursivity of the following nameservers : {names}.',
+        'RECURSIVITY_UNDEF'   => 'Cannot determine if the following servers are recursive nameservers or not: {names}.',
         'ANSWER_BAD_RCODE'    => 'Nameserver {ns}/{address} answered AAAA query with an unexpected rcode ({rcode}).',
         'EDNS0_BAD_ANSWER'    => 'Nameserver {ns}/{address} does not support EDNS0 (OPT not set in reply).',
         'EDNS0_SUPPORT'       => 'The following nameservers support EDNS0 : {names}.',
