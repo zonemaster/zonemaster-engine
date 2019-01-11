635fc0035b14df902279e9193cbaa3d23446168c
Vincent Levigneron <vincent.levigneron@afnic.fr>
2019-01-11 14:36:40 +0100

    - Add true/false processing for Booleans.

--- lib/Zonemaster/Engine/Profile.pm
+++ lib/Zonemaster/Engine/Profile.pm
@@ -215,12 +215,12 @@ sub _set {
     if ( ! $value_type  or $value_type eq q{SCALAR} ) {
         die "Property $property_name can not be undef" if not defined $value;
 
-        # Boolean (Should we accept (true|false) values ?
+        # Boolean
         if ( $profile_properties_details{$property_name}->{type} eq q{Bool} ) {
-            if ( $from eq q{DIRECT} and $value =~ /^0$/ ) {
+            if ( $from eq q{DIRECT} and ($value =~ /^0$/ or $value eq q{false}) ) {
                 $value = JSON::PP::false;
             }
-            elsif ( $from eq q{DIRECT} and $value =~ /^1$/ ) {
+            elsif ( $from eq q{DIRECT} and ($value =~ /^1$/ or $value eq q{true}) ) {
                 $value = JSON::PP::true;
             }
             elsif ( $from eq q{JSON} and $value_type and $value == JSON::PP::false ) {
