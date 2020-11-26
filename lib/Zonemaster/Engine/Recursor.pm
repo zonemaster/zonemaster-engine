7c35f9bc21353d0947040f38d2aaaa6cbc4e358b
Mats Dufberg <mats.dufberg@iis.se>
2020-11-26 15:46:08 +0100

    Sorted
    * Sorted by name server name
    * Name listed before address
    * IPv4 listed before IPv6

--- lib/Zonemaster/Engine/Recursor.pm
+++ lib/Zonemaster/Engine/Recursor.pm
@@ -380,84 +380,52 @@ __DATA__
 {
    "." : [
       {
-         "name" : "m.root-servers.net",
-         "address" : "202.12.27.33"
+         "name" : "a.root-servers.net",
+         "address" : "198.41.0.4"
       },
       {
-         "name" : "m.root-servers.net",
-         "address" : "2001:dc3:0:0:0:0:0:35"
-      },
-      {
-         "name" : "e.root-servers.net",
-         "address" : "192.203.230.10"
-      },
-      {
-         "address" : "199.7.83.42",
-         "name" : "l.root-servers.net"
-      },
-      {
-         "address" : "2001:500:3:0:0:0:0:42",
-         "name" : "l.root-servers.net"
-      },
-      {
-         "address" : "198.41.0.4",
-         "name" : "a.root-servers.net"
-      },
-      {
-         "address" : "2001:503:ba3e:0:0:0:2:30",
-         "name" : "a.root-servers.net"
-      },
-      {
-         "address" : "192.5.5.241",
-         "name" : "f.root-servers.net"
-      },
-      {
-         "address" : "2001:500:2f:0:0:0:0:f",
-         "name" : "f.root-servers.net"
-      },
-      {
-         "address" : "199.7.91.13",
-         "name" : "d.root-servers.net"
-      },
-      {
-         "address" : "2001:500:2d:0:0:0:0:d",
-         "name" : "d.root-servers.net"
-      },
-      {
-         "address" : "192.58.128.30",
-         "name" : "j.root-servers.net"
-      },
-      {
-         "address" : "2001:503:c27:0:0:0:2:30",
-         "name" : "j.root-servers.net"
-      },
-      {
-         "address" : "128.63.2.53",
-         "name" : "h.root-servers.net"
-      },
-      {
-         "name" : "h.root-servers.net",
-         "address" : "2001:500:1:0:0:0:803f:235"
-      },
-      {
-         "name" : "g.root-servers.net",
-         "address" : "192.112.36.4"
-      },
-      {
-         "name" : "k.root-servers.net",
-         "address" : "193.0.14.129"
-      },
-      {
-         "address" : "2001:7fd:0:0:0:0:0:1",
-         "name" : "k.root-servers.net"
+         "name" : "a.root-servers.net",
+         "address" : "2001:503:ba3e:0:0:0:2:30"
       },
       {
          "name" : "b.root-servers.net",
          "address" : "192.228.79.201"
       },
       {
-         "address" : "192.33.4.12",
-         "name" : "c.root-servers.net"
+         "name" : "c.root-servers.net",
+         "address" : "192.33.4.12"
+      },
+      {
+         "name" : "d.root-servers.net",
+         "address" : "199.7.91.13"
+      },
+      {
+         "name" : "d.root-servers.net",
+         "address" : "2001:500:2d:0:0:0:0:d"
+      },
+      {
+         "name" : "e.root-servers.net",
+         "address" : "192.203.230.10"
+      },
+      {
+         "name" : "f.root-servers.net",
+         "address" : "192.5.5.241"
+      },
+      {
+         "name" : "f.root-servers.net",
+         "address" : "2001:500:2f:0:0:0:0:f"
+      },
+      {
+         "name" : "g.root-servers.net",
+         "address" : "192.112.36.4"
+      },
+      {
+         "name" : "h.root-servers.net",
+         "address" : "128.63.2.53"
+      },
+      {
+         "name" : "h.root-servers.net",
+         "address" : "2001:500:1:0:0:0:803f:235"
       },
       {
          "name" : "i.root-servers.net",
@@ -466,6 +434,38 @@ __DATA__
       {
          "name" : "i.root-servers.net",
          "address" : "2001:7fe:0:0:0:0:0:53"
+      },
+      {
+         "name" : "j.root-servers.net",
+         "address" : "192.58.128.30"
+      },
+      {
+         "name" : "j.root-servers.net",
+         "address" : "2001:503:c27:0:0:0:2:30"
+      },
+      {
+         "name" : "k.root-servers.net",
+         "address" : "193.0.14.129"
+      },
+      {
+         "name" : "k.root-servers.net"
+         "address" : "2001:7fd:0:0:0:0:0:1",
+      },
+      {
+         "name" : "l.root-servers.net",
+         "address" : "199.7.83.42"
+      },
+      {
+         "name" : "l.root-servers.net",
+         "address" : "2001:500:3:0:0:0:0:42"
+      },
+      {
+         "name" : "m.root-servers.net",
+         "address" : "202.12.27.33"
+      },
+      {
+         "name" : "m.root-servers.net",
+         "address" : "2001:dc3:0:0:0:0:0:35"
       }
    ]
 }
