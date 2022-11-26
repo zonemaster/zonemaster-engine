dbbcbdd03c4671a33093fc3bdee97fcdf3e4d3a9
Mats Dufberg <mats.dufberg@iis.se>
2022-11-26 19:05:37 +0100

    Corrects the license statement

--- lib/Zonemaster/Engine.pm
+++ lib/Zonemaster/Engine.pm
@@ -418,12 +418,8 @@ Calle Dybedahl <calle at init.se>
 
 =head1 LICENSE
 
-This is free software, licensed under:
-
-The (three-clause) BSD License
-
-The full text of the license can be found in the
-F<LICENSE> file included with this distribution.
+This is free software. The full text of the license can
+be found in the F<LICENSE> file included with this distribution.
 
 =cut
 
