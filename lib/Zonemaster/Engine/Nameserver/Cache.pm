65ea249b7d407dcd74ab0bfba6e80d9a8e17b6d1
Alexandre Pion <pion@afnic.fr>
2023-02-13 18:54:55 +0100

    Use Class::Accessor instead of Moose

--- lib/Zonemaster/Engine/Nameserver/Cache.pm
+++ lib/Zonemaster/Engine/Nameserver/Cache.pm
@@ -5,37 +5,57 @@ use version; our $VERSION = version->declare("v1.0.4");
 use 5.014002;
 use warnings;
 
-use Moose;
+use Class::Accessor "antlers";
+use Carp qw( confess );
+use Scalar::Util qw( blessed );
+
 use Zonemaster::Engine;
 
 our %object_cache;
 
-has 'data' => ( is => 'ro', isa => 'HashRef[Maybe[Zonemaster::Engine::Packet]]', default => sub { {} } );
-has 'address' => ( is => 'ro', isa => 'Net::IP::XS', required => 1 );
+has 'data' => ( is => 'ro' );
+has 'address' => ( is => 'ro' );
 
-around 'new' => sub {
-    my $orig = shift;
-    my $self = shift;
+sub new {
+    my $proto = shift;
+    my $class = ref $proto || $proto;
+    my $attrs = shift;
 
-    my $obj = $self->$orig( @_ );
+    confess "Attribute \(address\) is required"
+        if !exists $attrs->{address};
 
-    if ( not exists $object_cache{ $obj->address->ip } ) {
-        Zonemaster::Engine->logger->add( CACHE_CREATED => { ip => $obj->address->ip } );
-        $object_cache{ $obj->address->ip } = $obj;
+    # Type coercions
+    $attrs->{address} = Net::IP::XS->new( $attrs->{address} )
+        if !blessed $attrs->{address} || !$attrs->{address}->isa( 'Net::IP::XS' );
+
+    # Type constraint
+    confess "Argument must be coercible into a Net::IP::XS: address"
+        if !$attrs->{address}->isa( 'Net::IP::XS' );
+    confess "Argument must be a HASHREF: data"
+        if exists $attrs->{data} && ref $attrs->{data} ne 'HASH';
+
+    # Default value
+    $attrs->{data} //= {};
+
+    my $ip = $attrs->{address}->ip;
+    if ( exists $object_cache{ $ip } ) {
+        Zonemaster::Engine->logger->add( CACHE_FETCHED => { ip => $ip } );
+        return $object_cache{ $ip };
     }
 
-    Zonemaster::Engine->logger->add( CACHE_FETCHED => { ip => $obj->address->ip } );
-    return $object_cache{ $obj->address->ip };
-};
+    my $obj = Class::Accessor::new( $class, $attrs );
+
+    Zonemaster::Engine->logger->add( CACHE_CREATED => { ip => $ip } );
+    $object_cache{ $ip } = $obj;
+
+    return $obj;
+}
 
 sub empty_cache {
     %object_cache = ();
 
     return;
 }
-
-no Moose;
-__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
 
 1;
 
@@ -64,6 +84,10 @@ A reference to a hash holding the cache of sent queries. Not meant for external 
 =head1 CLASS METHODS
 
 =over
+
+=item new
+
+Construct a new Cache object.
 
 =item empty_cache()
 
