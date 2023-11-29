65a62a8c3bad2f600245956000720291ff560027
[Thomas Green] <thomas.green@afnic.fr>
2023-11-29 18:01:37 +0100

    Document and update method '_emit_log()' in all Test modules
    - Move '_emit_log()' to the proper section in each Test modules
    - Add documentation for '_emit_log()' in each Test modules
    - Align implementation of 'Zonemaster::Engine::Test::Basic::_emit_log()' with its counterpart from other Test modules
    - Make 'Zonemaster::Engine::Test::Address::find_private_address()' an internal method

--- lib/Zonemaster/Engine/Test/Connectivity.pm
+++ lib/Zonemaster/Engine/Test/Connectivity.pm
@@ -18,8 +18,6 @@ use Zonemaster::Engine::Constants qw[:ip];
 use Zonemaster::Engine::TestMethods;
 use Zonemaster::Engine::Util;
 
-sub _emit_log { my ( $tag, $argref ) = @_; return Zonemaster::Engine->logger->add( $tag, $argref, 'Connectivity' ); }
-
 =head1 NAME
 
 Zonemaster::Engine::Test::Connectivity - Module implementing tests focused on name servers reachability
@@ -409,6 +407,25 @@ sub version {
 }
 
 =head1 INTERNAL METHODS
+
+=over
+
+=item _emit_log()
+
+    my $log_entry = _emit_log( $message_tag_string, $hash_ref );
+
+Adds a message to the L<logger|Zonemaster::Engine::Logger> for this module.
+See L<Zonemaster::Engine::Logger::Entry/add($tag, $argref, $module, $testcase)> for more details.
+
+Takes a string (message tag) and a reference to a hash (arguments).
+
+Returns a L<Zonemaster::Engine::Logger::Entry> object.
+
+=back
+
+=cut
+
+sub _emit_log { my ( $tag, $argref ) = @_; return Zonemaster::Engine->logger->add( $tag, $argref, 'Connectivity' ); }
 
 =over
 
