b791874dd6c2bd05a4c3ba3f193e7a26e12207b8
Marc van der Wal <marc.vanderwal@afnic.fr>
2026-06-09 16:56:11 +0200

    Revert "Remove obsolete TO_JSON methods"
    This reverts commit bb5ddfbbb51a56b3a5639a024d1cf7d00d0139d0.
    
    It turns out that these methods weren’t obsolete after all. In
    particular, the removal of the TO_JSON method in
    Zonemaster::Engine::DNSName caused some issues that weren’t caught in
    unit tests.
    
    If you run:
    
      $ zonemaster-cli --level info --test basic02 --no-ipv6 \
              zonemaster.net --json | jq
    
    you should see that on the second message, the "domain" argument is a
    domain name, and no longer "null".

--- lib/Zonemaster/Engine/DNSName.pm
+++ lib/Zonemaster/Engine/DNSName.pm
@@ -155,6 +155,12 @@ sub prepend {
     return $self->new( { labels => \@labels } );
 }
 
+sub TO_JSON {
+    my ( $self ) = @_;
+
+    return $self->string;
+}
+
 1;
 
 =head1 NAME
@@ -233,6 +239,10 @@ See also L<https://tools.ietf.org/html/rfc7719#section-6>.
 =item prepend($label)
 
 Returns a new L<Zonemaster::Engine::DNSName> object, representing the called one with the given label prepended.
+
+=item TO_JSON
+
+Helper method for JSON encoding.
 
 =back
 
